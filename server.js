1d57089b5ad73c9ef2e1cd5935aafefd5af7fe41
Eduard Ballweg <eduard.ballweg@gmail.com>
2026-09-07 19:04:00 +0200

    vvv

--- server.js
+++ server.js
@@ -102,4 +102,5 @@ app.get('/trigger-push', async (req, res) => {
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server läuft auf Port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server läuft auf Port ${PORT}`));
+// Statt server.listen(3000) schreiben Sie:
